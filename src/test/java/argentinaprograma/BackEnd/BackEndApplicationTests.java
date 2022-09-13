ddbc145cf0efcca7da853701f5a42c1edde24c66
Braian Marquez <braianmarq95@gmail.com>
2022-09-13 10:12:18 -0300

    primer commit

---
+++ src/test/java/argentinaprograma/BackEnd/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package argentinaprograma.BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
